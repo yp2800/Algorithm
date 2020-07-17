78630adc19fe86c5e3534b86f4df5601f9bb394e
peng.yang <peng.yang@pintec.com>
2020-07-17 18:09:33 +0800

    update

---
+++ 69-x的平方根/mySqrt.go
@@ -0,0 +1,29 @@
+package leetcode
+
+import "math"
+
+func mySqrt(x int) int {
+	l, r := 0, x
+	ans := -1
+	for l <= r {
+		mid := l + (r-l)/2
+		if mid*mid <= x {
+			ans = mid
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return ans
+}
+
+func mySqrt2(x int) int {
+	if x == 0 {
+		return 0
+	}
+	ans := int(math.Exp(0.5 * math.Log(float64(x))))
+	if (ans+1)*(ans+1) <= x {
+		return ans + 1
+	}
+	return ans
+}
