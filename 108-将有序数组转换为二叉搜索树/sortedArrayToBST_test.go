3d6242928903d9b6c6d1274580ed4f7982738d29
peng.yang <peng.yang@pintec.com>
2020-07-28 11:47:09 +0800

    update

---
+++ 108-将有序数组转换为二叉搜索树/sortedArrayToBST_test.go
@@ -0,0 +1,49 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_sortedArrayToBST(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		// TODO: Add test cases.
+		{"first test", args{
+			nums: []int{-10, -3, 0, 5, 9},
+		}, &TreeNode{
+			Val: 0,
+			Left: &TreeNode{
+				Val: -3,
+				Left: &TreeNode{
+					Val:   -10,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: nil,
+			},
+			Right: &TreeNode{
+				Val: 9,
+				Left: &TreeNode{
+					Val:   5,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: nil,
+			},
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortedArrayToBST(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortedArrayToBST() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
