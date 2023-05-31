6db9e225001160b222db9978d29749ba18d61029
peng.yang <peng.yang@yiducloud.cn>
2023-05-31 21:43:04 +0800

    fix 414

--- 414.第三大的数/thirdMax.go
+++ 414.第三大的数/thirdMax.go
@@ -1,6 +1,9 @@
 package leetcode
 
-import "sort"
+import (
+	"github.com/emirpasic/gods/trees/redblacktree"
+	"sort"
+)
 
 func thirdMax(nums []int) int {
 	sort.Sort(sort.Reverse(sort.IntSlice(nums)))
@@ -13,4 +16,36 @@ func thirdMax(nums []int) int {
 		}
 	}
 	return nums[0]
+}
+
+func thirdMax2(nums []int) int {
+	t := redblacktree.NewWithIntComparator()
+	for _, num := range nums {
+		t.Put(num, nil)
+		if t.Size() > 3 {
+			t.Remove(t.Left().Key)
+		}
+	}
+	if t.Size() == 3 {
+		return t.Left().Key.(int)
+	}
+	return t.Right().Key.(int)
+}
+
+func thirdMax3(nums []int) int {
+	var a, b, c *int
+	for _, num := range nums {
+		num := num
+		if a == nil || num > *a {
+			a, b, c = &num, a, b
+		} else if *a > num && (b == nil || num > *b) {
+			b, c = &num, b
+		} else if b != nil && *b > num && (c == nil || num > *c) {
+			c = &num
+		}
+	}
+	if c == nil {
+		return *a
+	}
+	return *c
 }
