72ae264506b363925ae79232341e0b30824a034f
peng.yang <peng.yang@yiducloud.cn>
2023-06-05 22:54:39 +0800

    add 459

---
+++ 459.重复的子字符串/repeatedSubstringPattern_test.go
@@ -0,0 +1,26 @@
+package leetcode
+
+import "testing"
+
+func Test_repeatedSubstringPattern(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{"first test", args{s: "abab"}, true},
+		{"second test", args{s: "aba"}, false},
+		{"first test", args{s: "abcabcabcabc"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := repeatedSubstringPattern(tt.args.s); got != tt.want {
+				t.Errorf("repeatedSubstringPattern() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
