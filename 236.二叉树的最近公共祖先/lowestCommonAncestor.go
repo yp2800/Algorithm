735c888acbb60e0ae507b61c26671c4b59ef7fe6
peng.yang <peng.yang@yiducloud.cn>
2023-06-28 17:41:37 +0800

    add 236

---
+++ 236.二叉树的最近公共祖先/lowestCommonAncestor.go
@@ -0,0 +1,60 @@
+package leetcode
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 递归
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == p.Val || root.Val == q.Val {
+		return root
+	}
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	}
+	if left == nil {
+		return right
+	}
+	return left
+}
+
+// 存储父节点
+func lowestCommonAncestor2(root, p, q *TreeNode) *TreeNode {
+	parent := map[int]*TreeNode{}
+	visited := map[int]bool{}
+
+	var dfs func(node *TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		if node.Left != nil {
+			parent[node.Left.Val] = node
+			dfs(node.Left)
+		}
+		if node.Right != nil {
+			parent[node.Right.Val] = node
+			dfs(node.Right)
+		}
+	}
+	dfs(root)
+	for p != nil {
+		visited[p.Val] = true
+		p = parent[p.Val]
+	}
+	for q != nil {
+		if visited[q.Val] {
+			return q
+		}
+		q = parent[q.Val]
+	}
+	return nil
+}
